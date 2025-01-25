ba122dffbc3aaa31ac0381a645bbd7522553c368
Guillaume Raille <guillaume.raille@icloud.com>
2025-01-25 18:28:22 +0100

    add a basic async tests for langchain

--- tests/test_langchain_adapter.py
+++ tests/test_langchain_adapter.py
@@ -1,9 +1,39 @@
 import asyncio
+from textwrap import dedent
 
 import pytest
+from mcp import StdioServerParameters
+
+from mcpadapt.core import MCPAdapt
+from mcpadapt.langchain_adapter import LangChainAdapter
 
 
 @pytest.mark.asyncio
 async def test_langchain_adapter():
-    await asyncio.sleep(1)
-    pass
+    # define a mcp server with a tool for this test
+    mcp_server_script = dedent(
+        '''
+        from mcp.server.fastmcp import FastMCP
+
+        mcp = FastMCP("Echo Server")
+
+        @mcp.tool()
+        def echo_tool(text: str) -> str:
+            """Echo the input text"""
+            return f"Echo: {text}"
+        
+        mcp.run()
+        '''
+    )
+
+    # test asynchronously tool invocation
+    async with MCPAdapt(
+        StdioServerParameters(
+            command="uv", args=["run", "python", "-c", mcp_server_script]
+        ),
+        LangChainAdapter(),
+    ) as tools:
+        assert len(tools) == 1  # we expect one tool as defined above
+        assert tools[0].name == "echo_tool"  # we expect the tool to be named echo_tool
+        response = await tools[0].ainvoke("hello")
+        assert response == "Echo: hello"  # we expect the tool to return "Echo: hello"
