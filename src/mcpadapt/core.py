3dda8c8060f7fe0be4f8390b92bc9d922218b953
joejoe2 <7013601@gmail.com>
2025-05-21 21:23:25 +0800

    support streamable-http

--- src/mcpadapt/core.py
+++ src/mcpadapt/core.py
@@ -16,6 +16,7 @@ import mcp
 from mcp import ClientSession, StdioServerParameters
 from mcp.client.sse import sse_client
 from mcp.client.stdio import stdio_client
+from mcp.client.streamable_http import streamablehttp_client
 
 
 class ToolAdapter(ABC):
@@ -95,7 +96,15 @@ async def mcptools(
     if isinstance(serverparams, StdioServerParameters):
         client = stdio_client(serverparams)
     elif isinstance(serverparams, dict):
-        client = sse_client(**serverparams)
+        transport = serverparams.pop("transport", "sse")
+        if transport == "sse":
+            client = sse_client(**serverparams)
+        elif transport == "streamable-http":
+            client = streamablehttp_client(**serverparams)
+        else:
+            raise ValueError(
+                f"Invalid transport, expected sse or streamable-http found `{transport}`"
+            )
     else:
         raise ValueError(
             f"Invalid serverparams, expected StdioServerParameters or dict found `{type(serverparams)}`"
@@ -107,7 +116,7 @@ async def mcptools(
     elif isinstance(client_session_timeout_seconds, timedelta):
         timeout = client_session_timeout_seconds
 
-    async with client as (read, write):
+    async with client as (read, write, *_):
         async with ClientSession(
             read,
             write,
